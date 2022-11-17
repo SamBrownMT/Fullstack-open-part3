ed3cd4226132dfa85ae711949dc1a01b74777cea
Sam <sam.brown@madetech.com>
2022-11-17 11:03:51 +0000

    add env variable for port

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.get('/info',(request,response) => {
 			<h1>${new Date()}</h1>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 	console.log(`Server running on ${PORT}`)
 })
