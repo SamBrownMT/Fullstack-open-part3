de5e44350449bb2da51100905647b220a152031d
Sam <sam.brown@madetech.com>
2022-11-28 15:20:00 +0000

    add fronted to backend, complete exercise 3.11

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('content', (req, res) => {
 })
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
