908a0d2436932a3a7fd376cb0255f5d327f537bb
Sam <sam.brown@madetech.com>
2022-12-05 15:58:25 +0000

    add put handler in controller/complete exercise 3.17

--- index.js
+++ index.js
@@ -54,6 +54,21 @@ app.get('/api/persons/:id',(request,response,next) => {
 	  })
 })
 
+app.put('/api/persons/:id',(request,response,next) => {
+	const body = request.body
+
+	const person = {
+		name: body.name,
+		number: body.number,
+	}
+
+	Person.findByIdAndUpdate(request.params.id, person, {new:true})
+		.then(updatedPerson => {
+			response.json(updatedPerson)
+		})
+		.catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (request,response) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
